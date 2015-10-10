ec2e4a18404ace92dd92521f1b5df0ec2188cbce
Bastian Mavrakis <bamavrakis@uc.cl>
2015-10-10 13:24:53 -0300

    Arreglos prueba lectura bmp

--- TestQDBMP/TestQDBMP.c
+++ TestQDBMP/TestQDBMP.c
@@ -8,14 +8,10 @@ int main( int argc, char* argv[] )
     UINT    width, height;
     UINT    x, y;
 
-    if ( argc != 3 )
-    {
-        fprintf( stderr, "Usage: %s <input file> <output file>\n", argv[ 0 ] );
-        return 0;
-    }
+
 
     /* Read an image file */
-    bmp = BMP_ReadFile( argv[ 1 ] );
+    bmp = BMP_ReadFile("test.bmp");
     BMP_CHECK_ERROR( stderr, -1 ); /* If an error has occurred, notify and exit */
 
     /* Get image's dimensions */
@@ -30,15 +26,14 @@ int main( int argc, char* argv[] )
             /* Get pixel's RGB values */
             BMP_GetPixelRGB( bmp, x, y, &r, &g, &b );
 
+            fprintf("r:%i, g:%i, b:%i \n",r,g,b);
+
             /* Invert RGB values */
-            BMP_SetPixelRGB( bmp, x, y, 255 - r, 255 - g, 255 - b );
+
         }
     }
 
     /* Save result */
-    BMP_WriteFile( bmp, argv[ 2 ] );
-    BMP_CHECK_ERROR( stderr, -2 );
-
     /* Free all memory allocated for the image */
     BMP_Free( bmp );
 
