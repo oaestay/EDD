d3f6bf1b279281f57a9d41f5e935a890cd2966d8
Bastian Mavrakis <bamavrakis@uc.cl>
2015-10-11 00:18:54 -0300

    EJALE

--- Tarea2/src/main.c
+++ Tarea2/src/main.c
@@ -48,7 +48,7 @@ int main(int argc, char const *argv[]) {
     inorderTraversal(&heap, 0);
     arbolito = initNode(heap.size);
 
-    Createtree(heap);
+    Createtree(&heap);
     deltree(&arbolito);
     BMP_Free(bmp);
     list_destroy(list);
