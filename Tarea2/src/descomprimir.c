40488c1dbbe51e989a0312e7e7d4e883bfa432a1
Bastian Mavrakis <bamavrakis@uc.cl>
2015-10-16 14:45:08 -0300

    hue hue

--- Tarea2/src/descomprimir.c
+++ Tarea2/src/descomprimir.c
@@ -20,18 +20,19 @@ Dictionary *LeerHeader(const char *path,int *width,int *height)
   filesize = ftell(file); // get current file pointer
   fseek(file, 0, SEEK_SET);
   unsigned char bytes [filesize];
-  unsigned char ints [4];
+
   for(i=0;i<filesize;i++)
   {
       bytes[i]=fgetc(file);
     }
 
   }
-  bytes[0]=ints[0];
-  bytes[1]=ints[1];
-  bytes[2]=ints[2];
-  bytes[3]=ints[3];
-  *width = *(int *)ints;
+
+
+  *width = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | (bytes[3]);
+  *height = (bytes[4] << 24) | (bytes[5] << 16) | (bytes[6] << 8) | (bytes[7]);
+  size = (bytes[8] << 16) | (bytes[9] << 8) | (bytes[10] << 8);
+  seplength = (bytes[11] << 16) | (bytes[12] << 8) | (bytes[13] << 8);
 
   //*height = *bytes[4]+*bytes[5]*256+*bytes[6]*256*256+*bytes[7]*256*256*256;
   //size = *bytes[8]+*bytes[9]*256+*bytes[10]*256*256;
@@ -39,9 +40,9 @@ Dictionary *LeerHeader(const char *path,int *width,int *height)
   //seplength = *bytes[11]+*bytes[12]*256+*bytes[13]*256*256;
 
   printf("%d\n",*width);
-  //printf("%d\n",*height);
-  //printf("%d\n",size);
-  //printf("%d\n",seplength);
+  printf("%d\n",*height);
+  printf("%d\n",size);
+  printf("%d\n",seplength);
   fclose(file);
   return dict;
 
